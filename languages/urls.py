ea51e1803f9cd2830c8eb743b5736051e5fd3908
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-20 00:45:44 +0300

    added more models and viewsets

--- languages/urls.py
+++ languages/urls.py
@@ -15,6 +15,8 @@ from rest_framework.routers import DefaultRouter
 router = routers.DefaultRouter()
 
 router.register('languages', views.LanguageView)
+router.register('paradigms', views.ParadigmView)
+router.register('programmers', views.ProgrammerView)
 #router.register('review-view', views.ReviewViewSet)
 #urlpatterns = router.urls
 
